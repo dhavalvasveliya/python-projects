9299dee90ea9dc9be60d1610f2a02c1a91c38260
Dhaval Vasveliya <47396042+dhavalvasveliya@users.noreply.github.com>
2023-04-14 11:46:14 -0400

    Added Clear screen logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,56 +1,69 @@
-# Define the game board as a list of empty strings
-board = [" " for _ in range(9)]
+import os
 
-# Function to print the game board
-def print_board():
-    print("|".join(board[0:3]))
-    print("-+-+-")
-    print("|".join(board[3:6]))
-    print("-+-+-")
-    print("|".join(board[6:9]))
+# Function to clear the screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-# Function to check if a player has won
-def check_win(player):
-    # Define the winning combinations as tuples of the board indices
-    wins = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
-    # Check if any of the winning combinations are all occupied by the player's symbol
-    for combo in wins:
-        if all(board[i] == player for i in combo):
+# Function to print the current state of the game board
+def print_board(board):
+    clear_screen()
+    print("     |     |")
+    print(f"  {board[0]}  |  {board[1]}  |  {board[2]}")
+    print("_____|_____|_____")
+    print("     |     |")
+    print(f"  {board[3]}  |  {board[4]}  |  {board[5]}")
+    print("_____|_____|_____")
+    print("     |     |")
+    print(f"  {board[6]}  |  {board[7]}  |  {board[8]}")
+    print("     |     |")
+
+# Function to get the player's move
+def get_move(player, board):
+    while True:
+        move = input(f"{player}, enter a position (1-9): ")
+        if move.isdigit() and int(move) in range(1, 10) and board[int(move)-1] == " ":
+            return int(move) - 1
+        else:
+            print("Invalid move. Please try again.")
+
+# Function to check if the game is over
+def game_over(board):
+    # Check rows for a win
+    for i in range(0, 9, 3):
+        if board[i] == board[i+1] == board[i+2] != " ":
             return True
+    # Check columns for a win
+    for i in range(3):
+        if board[i] == board[i+3] == board[i+6] != " ":
+            return True
+    # Check diagonals for a win
+    if board[0] == board[4] == board[8] != " " or board[2] == board[4] == board[6] != " ":
+        return True
+    # Check if the board is full
+    if " " not in board:
+        return True
+    # If none of the above conditions are met, the game is not over
     return False
 
 # Function to play the game
 def play_game():
-    # Define the players and their symbols
-    players = ["X", "O"]
-    # Define the index of the current player
-    current_player = 0
-    
+    # Initialize the board and the player
+    board = [" "] * 9
+    player = "X"
     # Loop until the game is over
-    while True:
-        print_board()
-        # Prompt the current player to make a move
-        move = input(f"Player {current_player+1}, enter your move (1-9): ")
-        # Convert the move to a board index
-        index = int(move) - 1
-        # Check if the move is valid (i.e., the board index is empty)
-        if board[index] == " ":
-            # Place the player's symbol on the board
-            board[index] = players[current_player]
-            # Check if the player has won
-            if check_win(players[current_player]):
-                print_board()
-                print(f"Player {current_player+1} wins!")
-                return
-            # Check if the board is full (i.e., the game is a tie)
-            elif all(square != " " for square in board):
-                print_board()
-                print("It's a tie!")
-                return
-            # Switch to the other player
-            current_player = 1 - current_player
-        else:
-            print("Invalid move. Try again.")
+    while not game_over(board):
+        print_board(board)
+        move = get_move(player, board)
+        board[move] = player
+        player = "O" if player == "X" else "X"
+    # Game is over, print the final board
+    print_board(board)
+    # Determine the winner or declare a tie
+    if " " not in board:
+        print("It's a tie!")
+    else:
+        winner = "X" if player == "O" else "O"
+        print(f"{winner} wins!")
 
-# Play the game
+# Example usage:
 play_game()
